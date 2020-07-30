a55ef417304b12e5007c16a5418c8911a3ec05b6
jh69 <jonhendren@gmail.com>
2020-07-29 20:38:17 -0700

    version 3.0 probably

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ohhellyeah
+
+You can run it manually or you can spin up a container using docker-compose. The world is your oyster
--- ohhellyeah.py
+++ ohhellyeah.py
@@ -2,12 +2,10 @@ import time
 from random import randint
 
 words = [
-    {"probability": 99, "characters": [("o", 95), ("h", 80)]},
-    {
-        "probability": 90,
-        "characters": [("hell ", 95), ("y", 95), ("e", 94), ("a", 90), ("h", 80)],
-    },
-    {"probability": 80, "characters": [("baby", 101), ("y", 60)]},
+    {"prob": 99, "chars": [("o", 95), ("h", 90)]},
+    {"prob": 90, "chars": [("hell", 101), ("l", 90)]},
+    {"prob": 95, "chars": [("yeah", 101), ("y", 95), ("e", 94), ("a", 90), ("h", 80)]},
+    {"prob": 80, "chars": [("baby", 101), ("y", 60)]},
 ]
 
 
@@ -16,38 +14,36 @@ def r(rnd=100):
 
 
 def createword(worddict):
-    if r() > worddict["probability"]:
+    if r() > worddict["prob"]:
         return ""
     returnthis = ""
-    for char in worddict["characters"]:
+
+    for char in worddict["chars"]:
         if len(char[0]) > 1:
             if r() < char[1]:
                 returnthis += char[0]
         else:
-            if char[1] > 89:
-                returnthis += char[0]
             while r() < char[1]:
                 returnthis += char[0]
     return returnthis
 
 
 def ohhellyeahbaby(wordsdict):
-    results = []
-    for word in wordsdict:
-        results.append(createword(word))
+    result = " ".join([createword(word) for word in wordsdict]).strip()
 
-    result = " ".join(results).strip()
     while "  " in result:
         result = result.replace("  ", " ")
 
     punctuation = "."
     if r() < 75:
         punctuation = "!"
-        while r() < 90:
-            result += punctuation
 
-    if r() < 50:
+    while r() < 90:
+        result += punctuation
+
+    if r() < 40:
         result = result.upper()
+
     return result
 
 
